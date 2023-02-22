f25ca35e882839769b4888987087ffc77e4ff116
Li Yingping <liyp.dl.cn@gmail.com>
2023-02-22 17:08:18 +0800

    bugfix: use cmdCheck cause exit

--- bootstrap.sh
+++ bootstrap.sh
@@ -47,7 +47,6 @@ warn() {
 
 error() {
 	msg "${Red}[✘]${Color_off} ${1}${2}"
-	exit 1
 }
 success() {
 	msg "${Green}[✔]${Color_off} ${1}${2}"
@@ -75,7 +74,7 @@ terminalEnv() {
 	case ${1} in
 	1)
 		info "Install zsh"
-		if cmdCheck zsh -eq 0; then
+		if cmdCheck zsh -eq 1; then
 			aptInstall zsh
 		fi
 		info "Install oh-my-zsh"
@@ -84,7 +83,7 @@ terminalEnv() {
 		;;
 	2)
 		info "Install tmux"
-		if cmdCheck tmux -eq 0; then
+		if cmdCheck tmux -eq 1; then
 			aptInstall tmux
 		fi
 		cp config/tmux.conf ~/.tmux.conf
@@ -116,7 +115,7 @@ rosDevelopmentEnv() {
 
 		# dependencies and build
 		aptInstall "python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential"
-		if cmdCheck pip3 -eq 0; then
+		if cmdCheck pip3 -eq 1; then
 			aptInstall "python3-pip"
 		fi
 		sudo pip3 install 6-rosdep
